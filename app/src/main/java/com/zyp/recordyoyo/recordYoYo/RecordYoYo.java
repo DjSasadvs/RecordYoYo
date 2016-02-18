0ffce0b93e81a1f84f63e59ec5a0f5dbd2e05bdf
YipengZ <zypgoole@gmail.com>
2016-02-18 10:49:05 +0800

    use Singleton patterns

--- app/src/main/java/com/zyp/recordyoyo/recordYoYo/RecordYoYo.java
+++ app/src/main/java/com/zyp/recordyoyo/recordYoYo/RecordYoYo.java
@@ -13,4 +13,14 @@ public class RecordYoYo {
     public static final int DRAW_SETTINGS = 6;
     //public static final int DRAW_REFLECTION = 5;
 
+    private RecordYoYo() {
+    }
+
+    private static class HolderClass {
+        private final static RecordYoYo instance = new RecordYoYo();
+    }
+
+    public static RecordYoYo getInstance() {
+        return HolderClass.instance;
+    }
 }
